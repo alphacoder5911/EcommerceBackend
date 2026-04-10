7af7e45b2f5fb8fe75654ff4590dc615e7b282c1
Shreyas Lalge <lalgeshreyas@gmail.com>
2026-04-10 14:02:06 +0530

    added http routes

--- internal/dto/auth.go
+++ internal/dto/auth.go
@@ -5,7 +5,7 @@ type RegisterRequest struct {
 	Email      string `json:"email" binding:"required,email"`
 	Password   string `json:"password"  binding:"required,min=8"`
 	FirstName  string `json:"first_name" binding:"required"`
-	SecondName string `json:"last_name" binding:"required"`
+	LastName string `json:"last_name" binding:"required"`
 	Phone      string `json:"phone" binding:"required"`
 }
 
